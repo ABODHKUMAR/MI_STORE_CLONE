3178d94bb9651bd5b592b9f7336fe845340bde70
Abodh <82598946+ABODHKUMAR@users.noreply.github.com>
2022-11-20 08:00:24 +0530

    Add files via upload

---
+++ MI_STORE_CLONE/precache-manifest.2ef2b08d2f6e8433da39326f081e62de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05389ec21a18cc6fa484",
+    "url": "/static/css/main.8b1fb65f.chunk.css"
+  },
+  {
+    "revision": "05389ec21a18cc6fa484",
+    "url": "/static/js/main.05389ec2.chunk.js"
+  },
+  {
+    "revision": "af3c0a2d4063417322a8",
+    "url": "/static/css/1.0b9bd57b.chunk.css"
+  },
+  {
+    "revision": "af3c0a2d4063417322a8",
+    "url": "/static/js/1.af3c0a2d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0fc194039a3a4382abe8b801f4029ec",
+    "url": "/index.html"
+  }
+];
